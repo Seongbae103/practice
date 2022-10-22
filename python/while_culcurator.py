4d1a7fe2439269a59c49362e1026c6fbb71b9ed1
Seongbae103 <seongbae103@gmail.com>
2022-10-22 18:25:53 +0900

    10-22

--- python/while_culcurator.py
+++ python/while_culcurator.py
@@ -3,18 +3,19 @@ from calendar import c
 from distutils.command.build_scripts import first_line_re
 
 
-class Culcurator(object):
+class Culcur(object):
     def __init__(self, fi, op, se) -> None:
         self.first = fi
         self.op = op
         self.second = se
-        self.print_result()
-    
+        self.culc_num()
+        
+        
     @staticmethod
     def set_num():
-        return Culcurator(int(input("첫번째 수 : ")),
-                          input("연산자 : "),
-                          int(input("두번째 수 : ")))
+        return Culcur(int(input("첫번째 수 : ")),
+                      input("연산자 : "),
+                      int(input("두번째 수 : ")))
     
     def culc_num(self):
         fi = self.first
@@ -38,11 +39,17 @@ class Culcurator(object):
         print(f"{self.first} {self.op} {self.second} = {self.result}")
 
     @staticmethod
+    def print_end(ls):
+        [i.print_result() for i in ls]
+
+    @staticmethod
     def main():
         ls = []
         while True:
-            Culcurator.set_num()
-            ls.append(Culcurator.set_num())
-            self.print_result()
-
-Culcurator.main()
+            Culcur.set_num()
+            print("연산내용 : ")
+            ls.append(Culcur.set_num())
+            print("결과 : ")
+            Culcur.print_end(ls)
+            break
+Culcur.main()
